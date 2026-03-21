608fc278ad39186e1f4de1ce279d0f8465a8d83a
Veyrost <142344499+Khayom1@users.noreply.github.com>
2026-03-21 21:01:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-import logging
+import os
+from supabase import create_client, Client
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 
-# Токени аз @BotFather гирифтаатонро дар байни нохунакҳо гузоред
-API_TOKEN = 'ИН_ҶО_ТОКЕН_ГУЗОРЕД'
+# Маълумот аз Supabase
+url: str = os.getenv("dwjgughlyefxkfpqnekc")
+key: str = os.getenv("sb_publishable_J-gOSzG40A3ulv6Wa6htHw_6f6A6cv7")
+supabase: Client = create_client(url, key)
 
-logging.basicConfig(level=logging.INFO)
-bot = Bot(token=API_TOKEN)
+# Маълумоти Бот
+bot = Bot(token=os.getenv("BOT_TOKEN"))
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['start'])
-async def cmd_start(message: types.Message):
-    await message.answer(f"Салом {message.from_user.first_name}! Ман боти шахсии Delta Force TJ ҳастам. Фармонро интизорам!")
+@dp.message_handler(commands=['register'])
+async def register_player(message: types.Message):
+    # Сабти бозингар дар базаи Supabase
+    user_id = message.from_user.id
+    username = message.from_user.username
+    
+    data, count = supabase.table("players").insert({
+        "tg_id": user_id, 
+        "name": username, 
+        "points": 0
+    }).execute()
+    
+    await message.answer("✅ Шумо дар базаи Delta Force TAJIKISTAN (TJ)🇹🇯 сабт шудед!")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-
